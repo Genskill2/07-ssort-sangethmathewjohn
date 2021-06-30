8ae54216b59ac10fcba7e636d78302cccaba9db0
Sangeeth Mathew John <65032296+sangethmathewjohn@users.noreply.github.com>
2021-06-30 15:48:58 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -4,11 +4,21 @@ int swap_max(int arr[], int l, int n ){
     int max = arr[0];
     int pos= n;
     for (int i=0; i<l; i++){
-        if arr[i] >arr[max]{
+        if (arr[i] >arr[max]){
             max = arr[i];
             pos = i;
         }
     }
     arr[max] =arr[n];
     arr[max = arr[n]];
+}
+
+void ssort(int arr[], int l)
+{
+    for (int i = 0; i < l; i++)
+    {
+        swap_max(a, l, i);
+    }
+
+    return 0;
 }
